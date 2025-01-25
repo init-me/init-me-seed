a4e7bd79eb8e7cd7670aac44c331e4d40ccffc6c
liudaojie <liudaojie@yy.com>
2025-01-25 18:14:16 +0800

    feat: 初始化 init-me-seed-node

---
+++ packages/init-me-seed-node/seeds/base/prettier.config.mjs
@@ -0,0 +1,5 @@
+import nodePrettierRule from 'eslint-config-yyl-node/prettier.config.mjs'
+/** @type {import('prettier').Config} */
+export default {
+  ...nodePrettierRule
+}
---
+++ packages/init-me-seed-node/src/index.ts
@@ -0,0 +1,11 @@
+import { InitMeSeed } from 'init-me-seed-types'
+import { initSeedConfig } from 'init-me-helper'
+import path from 'path'
+
+const SEED_PATH = path.join(__dirname, '../seeds')
+
+const seed: InitMeSeed.Config = initSeedConfig({
+  path: SEED_PATH
+})
+
+module.exports = seed
